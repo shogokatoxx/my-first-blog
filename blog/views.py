c0d816a01445641b3909c5aa7ca8393dafd4a5a1
shogo <syogo_br@yahoo.co.jp>
2019-04-13 05:40:41 +0000

    Modified templates to display posts form database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+#.はカレントディレクトリ、もしくはカレントアプリケーションのこと。
+#views.pyとmodels.pyは同じディレクトリに置いてあるので.で記述できる
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #{}のなかにテンプレートに渡す値を入れる
+    #{}のなかに引数を記述するときは名前と値をセットにする ''で囲んだほうが名前、後ろは値、クエリセットのこと
+    return render(request,'blog/post_list.html',{'posts':posts})
